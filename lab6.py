0457b56cb33858a660aee7cb6f9729686873706e
Wyattdmiller <Wyatt@MacBook-Pro.local>
2020-12-01 17:35:59 -0800

    structured english

--- lab6.py
+++ lab6.py
@@ -16,53 +16,6 @@ we iterate over using a loop three times and print the results to the user. As a
 
 """
 
-"""
-import random
-num = generate random number
-
-If num is greater than 15, 
-  then the result will be "Cherries"
-Otherwise if num is > 10, 
-  the result will be "Orange"
-Otherwise if num is > 5, 
-  the result will be "Plum"
-Otherwise if num is > 2, 
-  the result will be "Melon"
-Otherwise if num is > 1, 
-  the result will be "Bell"
-Otherwise
-  the result will be "Bar"
-
-loop three times
-  print the output (fruit) to the user
-
-"""
-
-import random
-
-def main():
-  for i in range(0, 3):
-    spin()
-
-def spin():
-  rand_num = random.randint(1, 20)
-  output = ""
-  if(rand_num > 15):
-    output = "Cherries"
-  elif(rand_num > 10):
-    output = "Orange"
-  elif(rand_num > 5):
-    output = "Plum"
-  elif(rand_num > 2):
-    output = "Melon"
-  elif(rand_num > 1):
-    output = "Bell"
-  else:
-    output = "Bar"
-
-  print(output, end=" ")
-
-main()
 
 
 
